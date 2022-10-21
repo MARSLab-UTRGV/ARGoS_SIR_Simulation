8d6b9eb38760055aae43b44461054280df4a6930
Ryan Luna <32585803+rluna319@users.noreply.github.com>
2022-10-21 14:50:28 -0500

    Delete build directory

--- build/controllers/SIR_diffusion/CMakeFiles/SIR_diffusion_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SIR_diffusion_autogen"
-  "SIR_diffusion_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SIR_diffusion_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
